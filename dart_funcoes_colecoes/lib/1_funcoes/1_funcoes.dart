e6b1a928dbe3f47e253c7fed1b46c5d540ac06b0
celenny <celennycs@gmail.com>
2023-02-28 17:49:13 -0300

    corrigindo nome da pasta

---
+++ dart_funcoes_colecoes/lib/1_funcoes/1_funcoes.dart
@@ -0,0 +1,10 @@
+void main() {
+  int num1 = 2;
+  int num2 = 3;
+
+  print('a soma é ${sumInt(num1, num2)}');
+}
+
+int sumInt(int n1, int n2) {
+  return n1 + n2;
+}
---
+++ dart_funcoes_colecoes/lib/1_funcoes/2_parametros.dart
@@ -0,0 +1,23 @@
+void main() {
+  // Parâmetros obrigatório por default
+  print('A soma = ${sumInt(10, 10)}');
+  // Parâmetros nomeados
+  // parametrs nmeados são nullabes por defalt
+  // e podem ser promovidos para non-null com checagem denull
+  print('A soma = ${sumDouble(n1: 10.5, n2: 9.5)}');
+  // Parâmetros obrigatórios
+  print('A soma = ${sumDoubleRequired(n1: 10.5, n2: 9.5)}');
+}
+
+int sumInt(int n1, int n2) {
+  return n1 + n2;
+}
+
+double sumDouble({double? n1, double? n2}) {
+  if (n1 != null && n2 != null) return n1 + n2;
+  return 0.0;
+}
+
+double sumDoubleRequired({required double n1, required double n2}) {
+  return n1 + n2;
+}
