a097e2e11e79324b9bc23a926d47d1b816d12462
celenny <celennycs@gmail.com>
2023-03-20 19:06:24 -0300

    dart consumo de api

--- e_dart_consumo_api/bin/e_dart_consumo_api.dart
+++
@@ -1,5 +0,0 @@
-import 'package:e_dart_consumo_api/e_dart_consumo_api.dart' as e_dart_consumo_api;
-
-void main(List<String> arguments) {
-  print('Hello world: ${e_dart_consumo_api.calculate()}!');
-}
